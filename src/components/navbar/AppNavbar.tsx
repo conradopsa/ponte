c44592ffd815c4b814a3e19ded9e9bb448d0286f
Conrado Pinheiro <conradopsa@gmail.com>
2021-01-16 18:36:49 -0300

    fix appNavbar

--- src/components/navbar/AppNavbar.tsx
+++ src/components/navbar/AppNavbar.tsx
@@ -1,16 +1,26 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useEffect, useRef, useState } from "react";
 
 interface AppNavbarProperties {
   position?: "absolute" | "fixed" | "relative" | "static" | any;
   [properties: string]: any;
 }
 
-export function AppNavbar({ position, children }: AppNavbarProperties) {
+export function AppNavbar({ style, children }: AppNavbarProperties) {
+  const sectionRef = useRef<HTMLElement | null>(null)
+  
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    setHeight(sectionRef.current?.clientHeight || 0)
+  }, [])
+
   return (
     <section
+      ref={sectionRef}
       style={{
         ...sectionStyle,
-        position: position || sectionStyle.position,
+        ...style,
+        marginBottom: `-${height}px`
       }}
     >
       <div style={divStyle}>{children}</div>
@@ -20,9 +30,9 @@ export function AppNavbar({ position, children }: AppNavbarProperties) {
 
 const sectionStyle: CSSProperties = {
   backgroundColor: "rgb(0, 0, 0, 0.15)",
-  position: "fixed",
+  position: "relative",
   top: 0,
-  right: 0,
+  left: 0,
   width: "100%",
   zIndex: 100
 };
