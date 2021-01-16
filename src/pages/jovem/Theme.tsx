689a083a12a06d09d5a4a6ef8e685cedbbc72362
gabriel <gabrielsynaii@gmail.com>
2021-01-16 09:11:53 -0300

    Added new theme to jovem routes

---
+++ src/pages/jovem/Theme.tsx
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#6C6AE2",
+    },
+    secondary: {
+      main: "#FFF",
+    },
+  },
+});
