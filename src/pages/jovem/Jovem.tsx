eb4180d73425a8c2a41af8deefefe854b8268138
gabriel <gabrielsynaii@gmail.com>
2021-01-16 08:40:01 -0300

    Added jovem routes

--- src/pages/jovem/Jovem.tsx
+++ src/pages/jovem/Jovem.tsx
@@ -1,7 +1,31 @@
-export function Jovem() {
+import React from 'react';
+import { Container, makeStyles } from "@material-ui/core";
+import { Switch } from "react-router-dom";
+import RouteWithSubRoutes from "../../components/RouteWithSubRoutes";
+
+const useStyles = makeStyles({
+  backgroundImage: {
+    display: 'inline-block',
+    width: '100vw',
+    height: '100vh',
+    backgroundImage: "url('/title-background.png')",
+    backgroundSize: '100% 53vh',
+    backgroundRepeat: 'no-repeat'
+  }
+});
+
+export function Jovem({ routes }: any) {
+  const classes = useStyles();
+
   return (
-    <>
-      <p>Jovem Home</p>
-    </>
+    <div className={classes.backgroundImage}>
+      <Container maxWidth="sm">
+        <Switch>
+          { routes.map((route: any, i: number) => (
+            <RouteWithSubRoutes key={i} {...route} />
+          ))}
+        </Switch>
+      </Container>
+    </div>
   );
 }
