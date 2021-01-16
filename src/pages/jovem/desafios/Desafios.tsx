f73b49ce1e024f0379cb12d1f83997cd5781cc17
gabriel <gabrielsynaii@gmail.com>
2021-01-16 11:09:10 -0300

    Added desafios page

--- src/pages/jovem/desafios/Desafios.tsx
+++ src/pages/jovem/desafios/Desafios.tsx
@@ -1,7 +1,55 @@
+import React from 'react';
+import { Grid, makeStyles, Typography } from '@material-ui/core';
+import { TrendingUp } from '@material-ui/icons';
+import { DesafiosInfo } from '../../../components/DesafiosInfo';
+import Description from '../../../components/Description';
+
+const useStyles = makeStyles({
+  grid: {
+    padding: '0px 8px'
+  },
+  title: {
+    alignSelf: 'flex-start'
+  }
+});
+
+const DESAFIOS = [
+  {
+    src: '/hackathonccr.png',
+    to: 'http://www.grupoccr.com.br/hackathonccr/'
+  }
+];
+
 export function JovemDesafios() {
+  const classes = useStyles();
+
   return (
     <>
-      <p>Jovem Desafios</p>
+      <Description>
+        <Typography align="center" variant="subtitle2" color="secondary">
+          Além da teoria <br/>
+          Aprenda na prática e potencialize sua formação
+        </Typography>
+        <TrendingUp color="primary" />
+      </Description>
+
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        className={classes.grid}
+        spacing={2}
+      >
+        <Grid item className={classes.title}>
+          <Typography variant="h5" color="secondary">Desafios disponíveis</Typography>
+        </Grid>
+
+        { DESAFIOS.map((elem, index) => (
+          <Grid item key={`desafio${index}`}>
+            <DesafiosInfo src={ elem.src } to={ elem.to } />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
